b25ed70b84e7c6a9d78791ea6e7d07fc02be8b20
george-alta <108602876+george-alta@users.noreply.github.com>
2023-04-02 07:51:49 +1200

    #D01 challenge completed

--- D01_band_name.py
+++ D01_band_name.py
@@ -1,2 +1,4 @@
 print("Welcome to the band name generator")
-city = input("What's the name of the city you grew in?")
+city = input("What's the name of the city you grew in?\n")
+pet = input("What's the name of your pet?\n")
+print("your band name is: " + city + " " + pet)
