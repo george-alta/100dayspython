6b3cecaad1c9d8b7483785c9e2121069c8adff03
george-alta <108602876+george-alta@users.noreply.github.com>
2023-04-03 06:09:46 +1200

    day 7 start

---
+++ D07_Hangman/hangman.py
@@ -0,0 +1,29 @@
+import random
+
+print(""" _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/      """)
+
+words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
+print(words)
+
+secret_word= words[random.randint(0,len(words)-1)]
+print(secret_word)
+letters = "abcdefghijklmnoprstuvwxyz"
+lives = 7
+while lives > 0:
+    guess = input("next letter: ")
+    guess[0]
+
+
