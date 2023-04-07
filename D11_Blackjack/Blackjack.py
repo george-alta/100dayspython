c994de09d8de9d35e67b00400700a96363643b39
george-alta <108602876+george-alta@users.noreply.github.com>
2023-04-07 17:09:05 +1200

    blackjack v2

--- D11_Blackjack/Blackjack.py
+++ D11_Blackjack/Blackjack.py
@@ -17,27 +17,21 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
 def drawcard():
-    return cards[random.randint(0,12)]
+    return random.choice(cards)
 player_score = 0
 computer_score = 0
 
 def computer_play(hand):
-    while sum(hand) < 17:
-        hand.append(drawcard())
+    while sum(hand) < 17: hand.append(drawcard())
     return hand
 
 def ace_in_hand(hand):
-    for card in range(len(hand)):
-        if hand[card] == 11:
-            return(True)
-    return False
+    return any(card == 11 for card in hand)
 
 def replace_ace(hand):
-    for card in range(len(hand)):
-        if hand[card] == 11:
-            hand[card] = 1
-            break
-    return (hand)
+    index = hand.index(11)
+    hand[index] = 1
+    return hand
 
 while True:
     os.system("cls")
@@ -48,15 +42,26 @@ while True:
     player_hand = [drawcard(),drawcard()]
     computer_hand = [drawcard(),drawcard()]
     player_lose = False
+    computer_lose = False
+    play_end = False
+    #check if computer gets blackjack
+    if sum(player_hand) == 21:
+        player_score += 1
+        print(f"BLACKJACK! you win 😄. Current Score: Human={player_score} CPU={computer_score}.")
+        continue
+    elif sum(computer_hand) == 21:
+        computer_score += 1
+        print(f"COMPUTER BLACKJACK! you lose 😄. Current Score: Human={player_score} CPU={computer_score}.")
+        continue
 
     print(f"Your Hand: {player_hand} - Total {sum(player_hand)}.")
     print(f"Computer Hand: [{computer_hand[0]} , _ ] \n\n")
     
-    while True:
+    while not player_lose and not computer_lose and not play_end:
         if sum(player_hand) == 21:
             player_score += 1
-            print(f"BLACKJACK! you win 😄. Current Score: {player_score} Human. {computer_score} CPU.")
-            break
+            print(f"BLACKJACK! you win 😄. Current Score:  Human={player_score} CPU={computer_score}.")
+            computer_lose = True
         next_play = input(f"Do you want to (d)raw another card or (s)tay? d/s: ")
         if next_play == "d":
             player_hand.append(drawcard())
@@ -65,29 +70,29 @@ while True:
             elif sum(player_hand)>21 and not ace_in_hand(player_hand):
                 computer_score += 1
                 player_lose = True
-                print(f"You lose: {sum(player_hand)} points 😭. Current Score: {player_score} Human. {computer_score} CPU.\n\n")
-                break
+                print(f"You lose: {sum(player_hand)} points 😭. Current Score: Human={player_score} CPU={computer_score}.\n")
             print(f"\nYour Hand: {player_hand} - Total {sum(player_hand)}.\n")
         if next_play == "s":
-            break
-    if not player_lose: # if player loses before this point, this code shouldn't be executed
+            play_end = True
+
+    if not player_lose and not computer_lose: # if player/computer loses before this point, this code shouldn't be executed
         player_sum = sum(player_hand)
         computer_hand = computer_play(computer_hand)
         computer_sum = sum(computer_hand)
-        print(f"\n --> Computer hand: {computer_hand}. Total {computer_sum}")
-        print(f" --> Human hand: {player_hand}. Total {player_sum}\n")
+        print(f"\n --> Human hand: {player_hand}. Total {player_sum}")
+        print(f"--> Computer hand: {computer_hand}. Total {computer_sum}\n")
         if computer_sum > 21 or computer_sum < player_sum:
             player_score +=1
-            print (f"Computer loses 😄. Current Score: {player_score} Human. {computer_score} CPU.\n\n")
+            print (f"Computer loses 😄. Current Score: Human={player_score} CPU={computer_score}.\n\n")
         elif computer_sum == player_sum:
-            print(f"It's a draw at {player_sum} points. Current Score: {player_score} Human. {computer_score} CPU.\n\n")
+            print(f"It's a draw at {player_sum} points. Current Score: Human={player_score} CPU={computer_score}.\n\n")
         else:
             computer_score += 1
-            print(f"computer wins 😭. Current Score: {player_score} Human. {computer_score} CPU.\n\n")
+            print(f"computer wins 😭. Human={player_score} CPU={computer_score}.\n\n")
 
 
     if input("do you want to play again? y/n: ") == "n": break
 
-print(f"End of game. Final score: {player_score} Human. {computer_score} CPU")
+print(f"End of game. Final score: Human={player_score} CPU={computer_score}.")
 
 ## TODO replace when there is an ace and score is over 21
