6382f3a133310dea7919bfec85a48968e6728750
george-alta <108602876+george-alta@users.noreply.github.com>
2023-04-16 18:17:53 +1200

    D20 working

---
+++ D20_Snake-day1/main.py
@@ -0,0 +1,23 @@
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    # for seg in snake_body:
+    #     seg.forward(20)
+
+
+screen.exitonclick()
