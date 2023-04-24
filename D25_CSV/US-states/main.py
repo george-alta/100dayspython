40d20d4effca08a3c8e3024b3de01679c0db51d9
george-alta <108602876+george-alta@users.noreply.github.com>
2023-04-25 08:01:33 +1200

    D25 Finish

--- D25_CSV/US-states/main.py
+++ D25_CSV/US-states/main.py
@@ -10,19 +10,24 @@ turtle.shape(image)
 
 data = pandas.read_csv("D25_CSV/US-states/50_states.csv")
 all_states = data["state"].to_list()
-print(data)
-states_guessed = 0
+states_guessed = []
 state_labels = []
-while states_guessed < 50:
+while len(states_guessed) < 50:
     answer_state = screen.textinput(
-        title=f"Guess a state {states_guessed}/50", prompt="Enter a name")
-    # answer_state = answer_state.lower()
-    print(answer_state)
+        title=f"Guess a state {states_guessed}/50", prompt="Enter a name").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in states_guessed:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("D25_CSV/US-states/states_to_learn.csv")
+        break
 
     # check if answer if part of the state list
     if answer_state in all_states:
-        data[data.state == answer_state]
-        states_guessed += 1
+        states_guessed.append(answer_state)
         new_turtle = turtle.Turtle()
         new_turtle.hideturtle()
         new_turtle.penup()
@@ -30,10 +35,6 @@ while states_guessed < 50:
         state_data = data[data.state == answer_state]
         state_x = int(state_data.x)
         state_y = int(state_data.y)
-        print(f"Coord: {state_x},{state_y}")
         new_turtle.goto(state_x, state_y)
         new_turtle.write(state_data.state.item())
         state_labels.append(new_turtle)
-
-
-screen.exitonclick()
