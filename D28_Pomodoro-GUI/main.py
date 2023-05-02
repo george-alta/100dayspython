a9a82698b7641d5c447eadedbf44e98c1c607184
george-alta <george.alta.nz@gmail.com>
2023-05-03 06:53:34 +1200

    D28 working

--- D28_Pomodoro-GUI/main.py
+++ D28_Pomodoro-GUI/main.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, Button, Label, Entry, Canvas, PhotoImage
-
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,7 +9,8 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
+checktext = ""
 # ---------------------------- TIMER RESET ------------------------------- #
 # testing
 
@@ -18,16 +19,49 @@ def func():
     pass
 
 
-def start():
-    pass
-
-
 def reset():
     pass
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    global reps
+    reps += 1
+    global checktext
+    work_sec = WORK_MIN * 60
+    break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 2 == 1:
+        countdown(work_sec)
+    elif reps % 8 == 0:
+        countdown(long_break_sec)
+    else:
+        countdown(break_sec)
+
+    while True:
+        countdown(work_sec)
+        # reps += 1
+        # for i in range(reps):
+        #     checktext += "✓"
+        checkmark.config(text=checktext)
+        if reps % 3 != 0:
+            countdown(break_sec)
+        else:
+            countdown(long_break_sec)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
+def countdown(count):
+    minutes = int(count/60)  # another option is using math.floor (import math)
+    seconds = count % 60
+    if seconds < 10:
+        canvas.itemconfig(timertext, text=f"{minutes}:0{seconds}")
+    else:
+        canvas.itemconfig(timertext, text=f"{minutes}:{seconds}")
+    if count > 0:
+        window.after(1000, countdown, count-1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -35,24 +69,27 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-title = Label(text="Timer", font=("Courier", 30, "bold"), fg=GREEN, bg=YELLOW)
-title.grid(column=1, row=0)
 
 canvas = Canvas(width=206, height=224, bg=YELLOW, highlightthickness=0)
+title = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+title.grid(column=1, row=0)
+
 tomato = PhotoImage(file="D28_Pomodoro-GUI/tomato.png")
 canvas.create_image(102, 112, image=tomato)
-canvas.create_text(103, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 35, "bold"))
+timertext = canvas.create_text(103, 130, text=f"{WORK_MIN}:00", fill="white",
+                               font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-button_start = Button(text="Start", command=start)
-button_start.grid(column=0, row=3)
-button_stop = Button(text="Reset", command=start)
-button_stop.grid(column=2, row=3)
+# countdown(5)
 
-checkmark = Label(text="✓", font=(
-    "Courier", 20, "bold"), fg=GREEN, bg=YELLOW)
-checkmark.grid(column=1, row=4)
+button_start = Button(text="Start", command=start_timer, highlightthickness=0)
+button_start.grid(column=0, row=2)
+button_reset = Button(text="Reset", command=reset, highlightthickness=0)
+button_reset.grid(column=2, row=2)
+
+checkmark = Label(text=checktext, font=(
+    FONT_NAME, 20), fg=GREEN, bg=YELLOW)
+checkmark.grid(column=1, row=3)
 
 
 window.mainloop()
