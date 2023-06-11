5b577b51a9ae061ba12a01f43b7c19495b55e3b0
george-alta <george.alta.nz@gmail.com>
2023-06-12 07:17:29 +1200

    D34 finish

--- D36_Stocks-Alerts/news.py
+++ D36_Stocks-Alerts/news.py
@@ -1,19 +1,20 @@
 from config import NEWSAPI_KEY
 import requests
 
-parameters = {"q": "tesla",
-              "apiKey": NEWSAPI_KEY,
-              "pageSize": 10}
 
-response2 = requests.get(
-    "https://newsapi.org/v2/everything", params=parameters)
-data = response2.json()
+def getnews():
+    parameters = {"q": "tesla",
+                  "apiKey": NEWSAPI_KEY,
+                  "pageSize": 10}
 
-for i in range(6):
-    source = response2.json()["articles"][i]["source"]["name"]
-    title = response2.json()["articles"][i]["title"].upper()
-    published_at = response2.json()["articles"][i]["publishedAt"][:10]
-    description = response2.json()["articles"][i]["description"]
+    response2 = requests.get(
+        "https://newsapi.org/v2/everything", params=parameters)
 
-    print(
-        f"Published in {source} on {published_at}:\n{title}\n\n{description}\n\n\n\n")
+    for i in range(6):
+        source = response2.json()["articles"][i]["source"]["name"]
+        title = response2.json()["articles"][i]["title"].upper()
+        published_at = response2.json()["articles"][i]["publishedAt"][:10]
+        description = response2.json()["articles"][i]["description"]
+
+        print(
+            f"{i+1}. {title} ({source} reported on {published_at})\n{description}\n")
