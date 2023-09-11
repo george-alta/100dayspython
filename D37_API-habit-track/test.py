b5ed00404d643b424ff9ffac66c138bea3b52845
george-alta <george.alta.nz@gmail.com>
2023-09-11 21:13:07 +1200

    D37 finished

--- D37_API-habit-track/test.py
+++ D37_API-habit-track/test.py
@@ -9,7 +9,7 @@ now = datetime.now()
 today = now.strftime("%Y%m%d")
 print(today)
 
-# delete a pixel
+# how to delete a pixel (2022 - 09 - 11)
 pixel_url = f'https://pixe.la/v1/users/{USERNAME}/graphs/graph1/20220911'
 response = requests.delete(url=pixel_url, headers=headers)
 print(response.text)
