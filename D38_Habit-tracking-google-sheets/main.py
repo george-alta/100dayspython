d956ae2bfacfa17328ba896dc6d2c147b0dcbbd0
george-alta <george.alta.nz@gmail.com>
2023-09-13 08:20:51 +1200

    D38 completed

--- D38_Habit-tracking-google-sheets/main.py
+++ D38_Habit-tracking-google-sheets/main.py
@@ -1,4 +1,4 @@
-from config import NUT_APP_ID, NUT_API_KEY, SHEETY_ENDPOINT
+from config import NUT_APP_ID, NUT_API_KEY, SHEETY_ENDPOINT, SHEETY_BEARER
 import requests
 from datetime import datetime
 
@@ -12,15 +12,15 @@ json_exercise = {
     "height_cm": 176,
     "age": 38
 }
-headers = {'x-app-id': NUT_APP_ID,
-           'x-app-key': NUT_API_KEY,
-           'Content-Type': 'application/json'
+headers_nutri = {'x-app-id': NUT_APP_ID,
+                 'x-app-key': NUT_API_KEY,
+                 'Content-Type': 'application/json'
 
-           }
+                 }
 
 
 response = requests.post(url=excercise_endpoint,
-                         json=json_exercise, headers=headers)
+                         json=json_exercise, headers=headers_nutri)
 response.raise_for_status()
 result = response.json()
 print(result)
@@ -29,14 +29,19 @@ json_post = {
     'workout': {
         'date': datetime.now().strftime("%d/%m/%Y"),
         'time': datetime.now().strftime("%H:%M:%S"),
-        'exercise': result['exercises'][0]['name'],
+        'exercise': result['exercises'][0]['name'].title(),
         'duration': result['exercises'][0]['duration_min'],
         'calories': result['exercises'][0]['nf_calories']
     }
 }
 
+headers_sheety = {
+    "Authorization": SHEETY_BEARER
+}
+
 print(json_post)
-response2 = requests.post(url=SHEETY_ENDPOINT, json=json_post)
+response2 = requests.post(
+    url=SHEETY_ENDPOINT, json=json_post, headers=headers_sheety)
 print(response2.text)
 
-# TODO send as date, and time instead of text starting with '
+# TODO #1 send as date, and time instead of text starting with '
