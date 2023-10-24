91231557af8f1b9c13c8708caa8d935164375c08
george-alta <george.alta.nz@gmail.com>
2023-10-24 13:37:21 +1300

    #D39

--- Days31to60/D39_flight_deal_finder/notification_manager.py
+++ Days31to60/D39_flight_deal_finder/notification_manager.py
@@ -1,3 +1,17 @@
+from config import TELEGRAM_BOT_KEY, CHAT_ID
+import requests
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        pass
+        # self.new_message = new_message
+
+    def send_message(self, message):
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_KEY}/sendMessage?chat_id={CHAT_ID}&text={message}"
+        requests.get(url).json()  # this sends the message
+
+
+# url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+# print(requests.get(url).json())
